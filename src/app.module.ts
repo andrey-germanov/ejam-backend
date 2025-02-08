f4c31767258553357e55e7138a51e1e3aa4264b9
andrey-germanov <crossfitgerman@gmail.com>
2025-02-09 01:39:47 +0200

    set up deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,8 @@ import { ThrottlerModule } from '@nestjs/throttler';
 import { SuperheroesModule } from './superheroes/superheroes.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Superhero } from './superheroes/superhero.entity';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 @Module({
   imports: [
@@ -10,13 +12,13 @@ import { Superhero } from './superheroes/superhero.entity';
     ThrottlerModule.forRoot([{ ttl: 60000, limit: 10 }]),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
+      host: process.env.DATABASE_HOST,
       port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'superheroes',
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
       entities: [Superhero],
-      synchronize: true,
+      synchronize: true, // turn off if you use this in production
     }),
   ],
 })
--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,6 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  console.log(process.env.FRONTEND_URL)
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
   
